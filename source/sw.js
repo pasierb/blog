efa6c4cf5bb0b27bfd1269641578ef6170ce77bb
Michal Pasierbski <mpasierbski@gmail.com>
2019-06-30 20:07:29 +0200

    add content cache

--- source/sw.js
+++ source/sw.js
@@ -1,6 +1,7 @@
-const CACHE_KEY = 'mpasierbski-v5';
+const RESOURCES_CACHE_KEY = 'mpasierbski-v5';
+const CONTENT_CACHE_KEY = 'mpasierbski-content-v1';
 
-const urlsToPrefetch = [
+const resourcesToPrefetch = [
   '/css/style.css',
   '/lib/font-awesome/css/font-awesome.min.css',
   '/js/main.js',
@@ -8,24 +9,61 @@ const urlsToPrefetch = [
   '/fonts/Ubuntu/Ubuntu-Regular.ttf'
 ];
 
+const contentToPrefetch = ['/', '/index.html'];
+
 self.addEventListener('install', function(event) {
+  // prefetch resources
   event.waitUntil(
-    caches.open(CACHE_KEY).then(function(cache) {
-      return cache.addAll(urlsToPrefetch);
-    }).catch(console.error)
+    Promise.all([
+      caches.open(RESOURCES_CACHE_KEY).then(function(cache) {
+        return cache.addAll(resourcesToPrefetch);
+      }),
+      caches.open(CONTENT_CACHE_KEY).then(function(cache) {
+        return cache.addAll(contentToPrefetch);
+      })
+    ])
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [
+    RESOURCES_CACHE_KEY,
+    CONTENT_CACHE_KEY
+  ];
+
+  // Delete unused caches
+  event.waitUntil(
+    caches.keys().then(function(cachesKeys) {
+      return Promise.all(cachesKeys.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_KEY).then(function(cache) {
-      return cache.match(event.request);
-    }).then(function(cachedResponse) {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request);
-    })
-  );
+  event.respondWith(handleFetch(event));
 });
+
+/**
+ * 
+ * @param {Event} event 
+ * @returns Promise
+ */
+function handleFetch(event) {
+  isContentRequest(event.request);
+
+  return caches.match(event.request).then(function(cachedResponse) {
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    return fetch(event.request);
+  });
+}
+
+function isContentRequest(request) {
+  console.log(request.url);
+}
